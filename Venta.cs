d442ad8a681efa6778471eab80ef65e6095c462e
QuimeyDelgado1 <133286540+QuimeyDelgado1@users.noreply.github.com>
2023-10-04 11:24:32 -0300

    Add files via upload

---
+++ Venta.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class Venta
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private int id;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private date fechaDeVenta;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private Cliente cliente;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private Producto[] productos;
+
+
+
+  #endregion
+
+
+  #region Public methods
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
