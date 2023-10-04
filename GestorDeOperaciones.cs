ebbd37746c74b2b48ce2596933aee317ab0b18ab
QuimeyDelgado1 <133286540+QuimeyDelgado1@users.noreply.github.com>
2023-10-04 11:34:36 -0300

    Add files via upload

---
+++ GestorDeOperaciones.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class GestorDeOperaciones
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+
+  #endregion
+
+
+  #region Public methods
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void RealizarVenta()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void GenerarInformeDeCompras()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void AgregarProductoAlInventario()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void RealizarCompra()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void GenerarInformeDeVenta()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
---
+++ Producto.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class Producto
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private int ID;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private string nombre;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private float precio;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private int stock;
+
+
+
+  #endregion
+
+
+  #region Public methods
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
