13ff389ca880f91746912daa94d97cfad32d989d
QuimeyDelgado1 <133286540+QuimeyDelgado1@users.noreply.github.com>
2023-10-04 12:35:32 -0300

    Add files via upload

---
+++ GestorDeVentas.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class GestorDeVentas
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+
+  #endregion
+
+
+  #region Public methods
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void RealizarVenta()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void AgregarProductoAlInventario()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void GenerarInformeDeVentas()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void new_operation()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
