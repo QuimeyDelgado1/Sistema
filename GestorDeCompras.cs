f9d99003f0f0ca2c5b1ae0398ddbb598e513f393
QuimeyDelgado1 <133286540+QuimeyDelgado1@users.noreply.github.com>
2023-10-04 12:33:35 -0300

    Add files via upload

---
+++ GestorDeCompras.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class GestorDeCompras
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+
+  #endregion
+
+
+  #region Public methods
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void RealizarCompra()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void AgregarProductoAlInventario()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public void GenerarInformeDeCompras()
+  {
+    throw new Exception("The method or operation is not implemented.");
+  }
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
